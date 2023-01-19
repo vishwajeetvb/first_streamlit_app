45762c37f06fcf5fcf91447aacd82d203557b252
Vishwajeet Behera <50745930+vishwajeetvb@users.noreply.github.com>
2023-01-19 13:42:43 +0530

    Update streamlit_app.py
    added a input

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+#added a input box
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 #It normalize the data of json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
